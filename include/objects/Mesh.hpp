12ca8f396da621e0327e597572f955d9ddddd1b0
Arthur Bartol <arthur.b4rtol@gmail.com>
2024-09-08 19:56:37 -0300

    Change: vector<Vector3> to vector<Vertex>

--- include/objects/Mesh.hpp
+++ include/objects/Mesh.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include "../math/Vector3.hpp"
+#include "../objects/Vertex.hpp"
 
 /**
   Represents a surface of a object by a list of points 
@@ -14,21 +15,10 @@ class Mesh {
         /**
            Adds a new vertex in a mesh 
         **/
-        void add_vertex(Vector3 vertice);
-
-        /**
-           Simple way to triangulate a polygon by choosing a vertex and drawing edges 
-           to all of the other vertices of the polygon
-        **/
-        void fan_triangulation();
+        void add_vertex(Vector3 position, Vector3 normal);
 
     private:
-        std::vector<Vector3> vertices;
-        std::vector<Vector3>  normals; //average normal at each vertice
-        std::vector<Vector3>  indices; //vertices indices
-        size_t              n_indices;
-
+        std::vector<Vertex> vertices;
+        std::vector<Vertex>  indices; //vertices indices
+        size_t             triangles;
 };
-
-
-
--- src/objects/Mesh.cpp
+++ src/objects/Mesh.cpp
@@ -1,16 +1,5 @@
 #include "../../include/objects/Mesh.hpp"
 
-void Mesh::add_vertex(Vector3 vertice) {
-    vertices.push_back(vertice);
+void Mesh::add_vertex(Vector3 position, Vector3 normal) {
+    vertices.push_back(Vertex(position, normal));
 }
-
-//TODO: fan_triangle
-/*
-void Mesh::fan_triangulation(std::vector<Vector3> vertices) {
-    std::vector<Vector3> triangles;
-    Vector3 triangle;
-
-    for(int i = 1; i < vertices.size() - 1; i++) {
-        triangle(vertices
-    }
-}*/
