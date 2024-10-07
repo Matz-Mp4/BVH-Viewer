a8249e78e6b50e776a8896dcdf065d7e372ef090
Matz-Mp4 <mateus.assalti@gmail.com>
2024-10-07 07:56:18 -0300

    Changed: Refactored Window  - Created a function in main to create a window

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,14 @@
 #include <iostream>
 #include <cmath>
 
-const unsigned int width = 800;
-const unsigned int height = 800;
+const unsigned int width = 600;
+const unsigned int height = 600;
 static float speed = 0.01f;
 static float sensitivity = 5.0f;
-static bool firstClick = true;
+static bool firstClick = false;
 static float prev_mouse_x = 0.0;
 static float prev_mouse_y = 0.0; 
+
 void handle_inputs(GLFWwindow* window, Camera& camera) {
     // Handles key inputs for world-space movement (Up, Back, Left, Right, Front)
     // Move up (along the global Y axis)
@@ -103,8 +104,8 @@ void handle_inputs(GLFWwindow* window, Camera& camera) {
         
     }
 }
-int main() {
-    
+
+GLFWwindow* create_window( unsigned int width, unsigned int height) {
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         exit(EXIT_FAILURE);
@@ -114,20 +115,28 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow* window = glfwCreateWindow(width, height, "Volumetric-Viewer", nullptr, nullptr);
     if (window == nullptr) {
-		std::cerr << "Your Window did the L" << std::endl;
+		std::cerr << "Could not open the Window" << std::endl;
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
     glfwMakeContextCurrent(window);
-    //Load Glew to it confuires OpenGL
     glewInit();
     glViewport(0, 0, width, height);
+    
+    return window;
+}
+
+
+int main() {
+    
+    GLFWwindow* window = create_window(width, height);
+
     std::string vertex_path = "../src/glsl-files/vertex.glsl";
     std::string frag_path = "../src/glsl-files/fragment.glsl";
     ShaderGLSL shader(vertex_path.c_str(), frag_path.c_str());
 
     /* GeometricObject object = GeometricObject(new Torus(Vector4(0.0 , 0.0, 0.0), 1.5, 0.5, 50, 50), RED_PLASTIC); */
-    GeometricObject object = GeometricObject(new ModelLoader("../models/happy_buddha.ply") , RED_PLASTIC);
+    GeometricObject object = GeometricObject(new ModelLoader("../models/bunny.ply") , RED_PLASTIC);
     /* GeometricObject object = GeometricObject(new Sphere(Vector4(0.0 , 0.0, 0.0), 1.5 , 50 , 50),RED_PLASTIC); */
     /* GeometricObject object = GeometricObject(new Cylinder(Vector4(0.0 , 0.0, 0.0), 0.5, 0.5), Material::RED_PLASTIC); */
     VAO vao;
@@ -160,7 +169,8 @@ int main() {
     // Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
     Camera camera(Vector4(0.0f, 0.0f,  5.0f, 1.0));
-    Matrix4 view_proj = camera.compute_view_projection(new PinHole(45, (float)width / height, 0.1f, 100.0f), CoordSystem::RIGH_HAND);
+    PinHole *pinhole_ptr = new PinHole(45, (float)width / height, 0.1f, 100.0f);
+    Matrix4 view_proj = camera.compute_view_projection(pinhole_ptr, CoordSystem::RIGH_HAND);
     unsigned int indices_size = object.get_indices().size();
     
     while (!glfwWindowShouldClose(window)){
@@ -171,24 +181,22 @@ int main() {
 		if (crntTime - prevTime >= 1.0/60 ){
             transform = Transformation::scaling(40.0, 40.0, 40.0) ;
             /* translate += step_trans; */
-            /* transform = Transformation::rotation_z(translate) ; */
+            /* transform = Transformation::rotation_z(translate) *; */
                         /* Transformation::rotation_x(translate) ; */
                         /* Transformation::rotation_y(translate)  ; */
 		}
         handle_inputs(window, camera);
-        view_proj = camera.compute_view_projection(new PinHole(90, (float)width / height, 0.1f, 100.0f), CoordSystem::RIGH_HAND);
+        view_proj = camera.compute_view_projection(pinhole_ptr, CoordSystem::RIGH_HAND);
         // Bind the VAO so OpenGL knows to use it
         shader.set_matrix4("transformation", transform);
         shader.set_matrix4("cameraProj", view_proj);
         vao.Bind();
         /* glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);  */
         glDrawElements(GL_TRIANGLES, indices_size, GL_UNSIGNED_INT, 0);
-        /* glDrawElements(GL_TRIANGLES,indices_size, GL_UNSIGNED_INT, 0); */
-        /* glDrawElements(GL_POINT, indices_size, GL_UNSIGNED_INT, 0); */
-            // Draw the crosshair
         glfwSwapBuffers(window);
         glfwPollEvents();
     } 
+    delete pinhole_ptr;
     vao.Delete();
     vbo.Delete();
     ebo.Delete();
